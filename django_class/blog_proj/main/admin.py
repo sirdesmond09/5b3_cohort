ee531aa843d3a78455d0e89c09b650f5a9a5d5ea
sirdesmond09 <sirdesmond09@gmail.com>
2022-07-05 13:51:03 +0100

    models, tests

--- django_class/blog_proj/main/admin.py
+++ django_class/blog_proj/main/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
-
+from .models import Post
 # Register your models here.
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("title", "slug", "author", "status","created")
+    list_filter = ("status", "created", "publish")
+    list_editable = ("status",)
+    search_fields = ("title", "slug", "body")
+    date_hierarchy = "publish"
+    search_help_text = "Search for posts your have created."
+    prepopulated_fields = {'slug': ('title',)}
+    
+    
--- django_class/blog_proj/main/tests.py
+++ django_class/blog_proj/main/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
-
+from .models import Post
+from django.contrib.auth.models import User
 # Create your tests here.
+
+
+class TestPostModel(TestCase):
+    
+    def test_create_model(self):
+        user = User.objects.create(username="testuser", password="testpassword123", email="test@gmail.com")
+        new_post = Post.objects.create(title="Test title", slug="test-slug", body="this is a test body", author=user)
+        all_post = Post.objects.all()
+        
+        self.assertEqual(new_post.title, "Test title")
+        self.assertEqual(new_post.body, "this is a test body")
+        self.assertEqual(new_post.author.id, user.id)
+        self.assertEqual(all_post.count(), 1)
+        self.assertIsInstance(new_post, Post)
