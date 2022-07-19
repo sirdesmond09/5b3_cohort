5038aaeac6c44674f34132047038fa87046ab8da
sirdesmond09 <sirdesmond09@gmail.com>
2022-07-19 15:01:05 +0100

    detail view

--- django_class/myapi_proj/main/urls.py
+++ django_class/myapi_proj/main/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path("employees/", views.EmployeeListView().as_view(), name="employee_list" ),
+    path("employees/<int:employee_id>/", views.EmployeeDetailView().as_view(), name="employee_detail" ),
 ]
--- django_class/myapi_proj/main/views.py
+++ django_class/myapi_proj/main/views.py
@@ -6,7 +6,7 @@ from .serializers import EmployeeSerializer
 from .models import Employee
 from rest_framework.views import APIView; 
 from drf_yasg.utils import swagger_auto_schema
-
+from rest_framework.exceptions import NotFound, PermissionDenied
 
 class EmployeeListView(APIView):
     
@@ -53,3 +53,60 @@ class EmployeeListView(APIView):
             return Response(data, status=status.HTTP_400_BAD_REQUEST)
         
     
+    
+
+class EmployeeDetailView(APIView):
+    
+    def get_object(self, employee_id):
+        try:
+            return Employee.objects.get(id=employee_id)
+        except Employee.DoesNotExist:
+            raise NotFound(detail={"message":"Employee not found"}, code=status.HTTP_404_NOT_FOUND)
+        
+        
+    def get(self, request, employee_id, format=None ):
+        """Api view to get the details of an employee"""
+        obj  =  self.get_object(employee_id)
+        serializer = EmployeeSerializer(obj)
+        data = {
+            "message" : "success",
+            "data": serializer.data
+        }
+        
+        return Response(data, status=status.HTTP_200_OK)
+    
+    
+    @swagger_auto_schema(method="put", request_body=EmployeeSerializer())
+    @action(methods=["put"], detail=True)
+    def put(self, request, employee_id, format=None):
+        """API View to edit employees"""
+        
+        obj  =  self.get_object(employee_id)
+        serializer = EmployeeSerializer(obj, data=request.data, partial=True)
+        
+        if serializer.is_valid():
+            if "employee_num" in serializer.validated_data.keys():
+                raise PermissionDenied(detail={"message":"you cannot edit your employee number"}, code=status.HTTP_403_FORBIDDEN)
+            
+            serializer.save()
+            
+            data = {
+                "message":"success",
+            }
+            return Response(data, status=status.HTTP_202_ACCEPTED)
+            
+
+        else:
+            data = {
+                "message":"failed",
+                "error":serializer.errors
+            }
+            return Response(data, status=status.HTTP_400_BAD_REQUEST)
+        
+        
+    def delete(self, request, employee_id, format=None):
+        """Delete an employee"""
+        
+        obj  =  self.get_object(employee_id)
+        obj.delete()
+        return Response({}, status=status.HTTP_204_NO_CONTENT)
